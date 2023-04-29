f6c0378cc25a8895723608294e2881206c514773
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2023-04-29 12:00:54 +0300

    Artem-name links were created and base structure for static files usage was added

--- project/main/urls.py
+++ project/main/urls.py
@@ -4,7 +4,7 @@ from . import views # Import our views.py file
 
 urlpatterns = [
     # If user gos to main page, we need to open the specified files from views.py
-    path('', views.home), # Call our methon from views.py where we have index() method.
-    path('registration/', views.registration,),
-    path('authorization/', views.authorization)
+    path('', views.home, name='home'), # Call our methon from views.py where we have index() method.
+    path('registration/', views.registration, name='registration'),
+    path('authorization/', views.authorization, name='authorization')
 ]
