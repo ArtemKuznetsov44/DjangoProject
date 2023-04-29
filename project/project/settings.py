f1e2565be08ea6d669b1f3ce4246fd1728a49424
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2023-04-29 09:48:42 +0300

    Artem-base templetes and one layout structure intit

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main', # Add our new app to this list of installed apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
