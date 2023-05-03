f0df193f6f9c08eeac99416dd7c738c1894c272c
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2023-05-03 21:29:10 +0300

    Artem-model structure, form structure and static connection were modifed

---
+++ project/main/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import User
+from django.forms.widgets import EmailInput, PasswordInput, TextInput, CheckboxInput, Select, RadioSelect
+
+
+class UserRegistrationForm(forms.ModelForm):
+    accept_rules = forms.BooleanField(required=True ,widget=CheckboxInput(attrs={"class": "form-check-input"}))
+    gender = forms.ChoiceField(choices=(('man', 'Man'), ('woman', 'Woman')), widget=RadioSelect)
+    age = forms.ChoiceField(choices=(('under_18', 'Under 18 years old'), ('over_18', 'Over 18 years old')), widget=Select(attrs={"class": "form-select form-select-sm"}))
+
+    class Meta:
+        model = User
+        fields = ['first_name',
+                  'middle_name',
+                  'email',
+                  'login',
+                  'password',
+                  'age',
+                  'gender',
+                  'to_be_informed',
+                  'accept_rules']
+
+
+        widgets = {
+            "first_name": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Your first name"
+            }),
+            "middle_name": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Your middle name"
+            }),
+            "email": EmailInput(attrs={
+                "class": "form-control",
+                "placeholder": "email@gmail.com"
+            }),
+            "login": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Your login"
+            }),
+            "password": PasswordInput(attrs={
+                "class": "form-control",
+                "placeholder": "Your password"
+            }),
+            "to_be_informed": CheckboxInput(attrs={
+                "class": "form-check-input"
+            }),
+        }
--- project/main/views.py
+++ project/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-# from django.http import HttpResponse # This class is used for generate HTTP-fromat responses (answers)
+from .forms import UserRegistrationForm
+# from django.http import HttpResponse # This class is used for generate HTTP-format responses (answers)
 
 # Create your views here.
 
-# To my mind it is like an action in Yii (maybe it is a worse statment)
+# To my mind it is like an action in Yii (maybe it is a worse statement)
 # The request param is required (обязетельный):
 def home(request): 
     # return HttpResponse("<h1>Main page</h1>")
@@ -12,7 +13,13 @@ def home(request):
     # pass
     
 def registration(request): 
-    return render(request, template_name='main/registration.html') 
+    form = UserRegistrationForm()
+    
+    data = {
+        'form': form,
+    }
+    
+    return render(request, template_name='main/registration.html', context=data) 
 
 def authorization(request): 
     return render(request, template_name='main/authorization.html')
