d9415737883f405f5ee54350322e308bdd25dabe
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2023-05-17 17:17:21 +0300

    Artem-fixing templates

--- project/main/urls.py
+++ project/main/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('registration/', UserRegister.as_view(), name='registration'), 
     path('authorization/', UserLogin.as_view(), name='authorization'),
     path('signout/', UserSignOut.as_view(), name='sign_out'),
-    path('profile/<int:pk>/', Profile.as_view(), name='profile'),
+    path('profile/id=<int:pk>/', Profile.as_view(), name='profile'),
     path('add_post/', AddNewPost.as_view(), name='new_post'), 
     
     # These are previous paths for registration and authentification for user without class-based views:
